dc9e8e9b5090d7962c2f980f2d1b51ba370b61e9
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-24 06:49:47 -0300

    feat(auth): implement sign-in form with validation

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,8 +2,27 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  async function handleSihnIn(data: any) {
+    console.log(data);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,13 +37,17 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSihnIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
-            <Button className="w-full cursor-pointer" type="submit">
+            <Button
+              disabled={isSubmitting}
+              className="w-full cursor-pointer"
+              type="submit"
+            >
               Acessar painel
             </Button>
           </form>
