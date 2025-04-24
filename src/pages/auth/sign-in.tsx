69ddb36f0c419454c9076ec532744c93017d10ba
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-24 07:11:49 -0300

    feat(auth): add toast notifications for sign-in process

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -4,6 +4,7 @@ import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -19,8 +20,20 @@ export function SignIn() {
   } = useForm<SignInForm>();
 
   async function handleSihnIn(data: any) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    try {
+      console.log(data);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Enviamos um link de autenticação para o seu e-mail.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSihnIn(data),
+        },
+      });
+    } catch (err) {
+      console.error(err);
+      toast.error("Credenciais inválidas.");
+    }
   }
 
   return (
